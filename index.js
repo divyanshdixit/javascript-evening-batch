a4138c93f0c0456f0234d4dbf7c5d54023a51b6f
divyanshdixitmds <divyansh@metadesignsolutions.co.uk>
2021-05-31 22:14:44 +0530

    js intro and operators"

---
+++ index.js
@@ -0,0 +1,79 @@
+// Statements:
+
+var a = 10; var b= 20; // declartion, definition
+var c;
+c = a + b;
+
+console.log(c);
+
+// =============
+
+//  JS keywords:
+
+/*
+var, const, let
+function,
+break
+continue
+for
+foreach
+
+*/
+
+
+// ===========
+
+// Js syntax:
+
+// declare variable => var a;
+// assign values => a = 10
+// computation => console.log(a)
+
+// 2 types of values
+
+// fixed: numbers, string 
+
+// variables: 
+
+// " ", ' '
+
+// $, letter, _
+
+// js case senstivie (var name , var Name)
+
+
+// ==== 
+
+//  +, -, /, *, %, **(exponentation), ++, --
+
+console.log(2**3)
+
+// =, !=, <=, >=
+
+//  string operator (+ to concatenate two string or one string and one number)
+var x = 10;
+var a = 30;
+var y = "Divyansh";
+var z = 20;
+var c = 20;
+
+console.log(x+a+y+z+c) // 40Divyansh20
+
+
+
+// ==== 
+
+// Js variables: var, let, const(to work a variable as private)
+
+{
+    let x = 10; // block level
+    console.log(x);
+
+}
+
+console.log(c);
+
+const f = "some";
+f = "new"
+console.log(f);
+
