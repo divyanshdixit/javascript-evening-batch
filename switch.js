cb96298059912acfcd4c86385a4ede6d0eddd938
divyanshdixitmds <divyansh@metadesignsolutions.co.uk>
2021-07-07 23:17:46 +0530

    switch case

---
+++ switch.js
@@ -0,0 +1,31 @@
+switch(expression){
+    // default:
+    //     // code
+    //     break;
+    case value to be compared:
+        // code
+        break;
+    case anothercase:
+        // code
+        break;
+    default: 
+        // code
+
+}
+
+// common code for different case 
+let x = 5
+
+switch(x){
+    case 4:
+    case 5:
+        console.log(x);
+        break;
+    case 6:
+        // code
+    case 7:
+    case 8:
+        // code
+}
+
+
